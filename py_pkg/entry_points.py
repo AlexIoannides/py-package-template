ffbd6a0989060001514a096808a978b8764b1f78
alexioannides <alex.ioanndies@yahoo.co.uk>
2018-11-09 13:21:59 +0000

    Updated download URL

--- py_pkg/entry_points.py
+++ py_pkg/entry_points.py
@@ -40,8 +40,8 @@ def install_template_from_github() -> None:
         return None
 
     # download ZIP archive of GitHub repository
-    py_pkg_github_url = 'https://github.com/AlexIoannides/pyspark-example-project/archive/master.zip'
-    r = requests.get(py_pkg_github_url)
+    url = 'https://github.com/AlexIoannides/py-package-template/archive/master.zip'
+    r = requests.get(url)
     with open('temp.zip', 'wb') as f:
         f.write(r.content)
 
