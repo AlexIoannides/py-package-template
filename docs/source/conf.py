4cd82cf210d9464b291e92b7b3ebed7c70c66e89
alexioannides <alex.ioanndies@yahoo.co.uk>
2018-11-10 00:23:59 +0000

    Added automatic versioning in docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../..'))
+from py_pkg.__version__ import __version__
 
 
 # -- Project information -----------------------------------------------------
@@ -24,9 +26,9 @@ copyright = '2018, Alex Ioannides'
 author = 'Alex Ioannides'
 
 # The short X.Y version
-version = ''
+version = __version__[:-2]
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = __version__
 
 
 # -- General configuration ---------------------------------------------------
--- py_pkg/__version__.py
+++ py_pkg/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the py-package-template package.
 
 __title__ = 'py-package-template'
 __description__ = 'py-package-template - Package Template Project Generator'
-__version__ = '0.1.2.999'
+__version__ = '0.2.0'
 __author__ = 'Alex Ioannides'
 __author_email__ = 'alex.ioannides@yahoo.co.uk'
 __license__ = 'Apache 2.0'
