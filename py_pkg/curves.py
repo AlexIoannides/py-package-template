d38fc412292a40caade3e368b76a678fc9952c63
RKiselev <ximeg@users.noreply.github.com>
2020-06-29 22:44:13 -0500

    Typo

--- py_pkg/curves.py
+++ py_pkg/curves.py
@@ -2,7 +2,7 @@
 py_pkg.curves.py
 ~~~~~~~~~~~~~~~~
 
-This exaple module contains classes that model economic supply and
+This example module contains classes that model economic supply and
 demand curves and compute values such as equilibrium prices. 
 
 It also serves as a demonstration for using type annotations and
